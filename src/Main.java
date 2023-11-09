080d36e7b9ffd85772107ef6004f3a451c894605
GOSIK <korotkevich.glebka@gmail.com>
2023-11-09 20:05:11 +0300

    Lesson_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        printThreeWords();
+        checkSumSing();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void checkSumSing() {
+        int a = 5, b = -14;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 150;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        } else {
+            System.out.println("Желтый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 9, b = 9;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
     }
 }
