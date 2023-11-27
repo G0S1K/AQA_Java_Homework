85278ffd84e9a888c4ba41ac572ae974cd06739d
GOSIK <korotkevich.glebka@gmail.com>
2023-11-27 15:14:59 +0300

    Lesson 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        String[][] array = new String[][]{
+                {"1", "3", "4", "5"},
+                {"0", "6", "0o", "8"},
+                {"3", "6", "7", "8"},
+                {"3", "5", "8", "9"}
+        };
+        try {
+            System.out.println(getSum(array));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int getSum(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array.length != 4 || array[i].length != 4 ) throw new MyArraySizeException("Array size isn't 4x4");
+            for (int j = 0; j < array[i].length; j++) {
+                try{
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Parsing exception at [" + i + ", " + j + "]");
+                }
+            }
+        }
+        return sum;
     }
 }
