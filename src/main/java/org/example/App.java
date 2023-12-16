d90b03181381b4989f59ba785a3494e82fd9d1ca
GOSIK <korotkevich.glebka@gmail.com>
2023-12-17 00:07:53 +0300

    Lesson_13

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
@@ -8,11 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(factorial(0));
-    }
-
-    public static int factorial(int number) {
-        if (number < 0 ) throw new IllegalArgumentException("Number must be positive");
-        return number == 0 ? 1 : number * factorial(number - 1);
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,25 +1,76 @@
 package org.example;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
 
-public class AppTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AppTest
+{
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.mts.by/");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        WebElement cookieAccept = driver.findElement(
+                By.xpath("//button[@class='btn btn_black cookie__ok']"));
+        cookieAccept.click();
+    };
     @Test
-    @DisplayName("Test with negative number")
-    void testWithNegativeNumber() {
-        assertThrows(IllegalArgumentException.class, () -> App.factorial(-4), "Number must be positive");
+    @Order(1)
+    void testNameOfBlock() {
+        WebElement nameOfBlock = driver.findElement(By.xpath("//div[@class='pay__wrapper']//h2"));
+        Assertions.assertEquals(nameOfBlock.getAttribute("textContent"), "Онлайн пополнение без комиссии",
+                "Name is empty");
     }
 
     @Test
-    @DisplayName("Test with positive numbers")
-    void testWithPositiveNumbers() {
-        assertEquals(1,App.factorial(1));
-        assertEquals(1,App.factorial(0));
-        assertEquals(120,App.factorial(5));
-        assertEquals(362880,App.factorial(9));
+    @Order(2)
+    void testCheckForSystems() {
+        WebElement paySystems = driver.findElement(By.xpath("//div[@class='pay__partners']//ul"));
+        Assertions.assertNotEquals(0, Integer.parseInt(paySystems.getAttribute("childElementCount")),
+                "There are no paysystems.");
     }
 
+    @Test
+    @Order(3)
+    void testLink() {
+        WebElement link = driver.findElement(By.xpath("//div[@class='pay__wrapper']//a"));
+        link.click();
+        Assertions.assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/",
+                driver.getCurrentUrl(), "This is wrong link");
+    }
 
+    @Test
+    @Order(4)
+    void testForm() throws InterruptedException {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInput = driver.findElement(
+                By.xpath("//form[@id='pay-connection']//div//input[@class='phone']"));
+        phoneInput.sendKeys("297777777");
+        WebElement totalRubInput = driver.findElement(
+                By.xpath("//form[@id='pay-connection']//div//input[@class='total_rub']"));
+        totalRubInput.sendKeys("10");
+        WebElement emailInput = driver.findElement(
+                By.xpath("//form[@id='pay-connection']//div//input[@class='email']"));
+        emailInput.sendKeys("ko.gl@gmail.com");
+        WebElement button = driver.findElement(
+                By.xpath("//form[@id='pay-connection']//button"));
+        button.click();
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        driver.close();
+    }
 }
